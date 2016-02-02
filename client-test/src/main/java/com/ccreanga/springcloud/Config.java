749c5fb623280c43cf575322a7fb494cb00b60e5
cornelcreanga <cornelcreanga@yahoo.com>
2016-02-02 16:43:16 +0100

    re-versioning

--- client-test/src/main/java/com/ccreanga/springcloud/Config.java
+++ client-test/src/main/java/com/ccreanga/springcloud/Config.java
@@ -3,14 +3,9 @@ package com.ccreanga.springcloud;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.endpoint.RefreshEndpoint;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @EnableAutoConfiguration
@@ -23,26 +18,9 @@ public class Config {
     @Autowired
     RefreshEndpoint refreshEndpoint;
 
-
-    @RefreshScope
-    @Bean
-    public Map hBean(){
-        System.out.println(smsService);
-        return  new HashMap();
-    }
-
     public String getSmsService() {
         return smsService;
     }
-
-
-//    @Scheduled(fixedRate=10000)
-//    public void run() {
-//        System.out.println("refreshed called");
-//        refreshEndpoint.refresh();
-//        System.out.println("done");
-//    }
-
 
 
 }
